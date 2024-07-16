3b558302b305dc88faf4881aede5de6242089997
Nasra Omar <Nasra.sharif.omar@gmail.com>
2024-07-16 14:09:53 -0500

    Created a function and added music collection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,66 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
+
+function addToCollection(collection, title, artist, yearPublished) {
+  let newAlbum = {
+  collection: collection, 
+  title: title,
+  artist: artist, 
+  yearPublished: yearPublished, 
+};
+
+collection.push(newAlbum);
+return newAlbum;
+}
+
+
+addToCollection(myCollection, "II", "Boyz II Men", 1991);
+addToCollection(myCollection, "Here My Dear", "Marvin Gaye", 1978);
+addToCollection(myCollection, "CTRL", "SZA", 2017);
+addToCollection(myCollection, "400 Degreez", "Juvenile", 1998);
+addToCollection(myCollection, "BLACKsummers night", "Maxwell", 2009);
+addToCollection(myCollection, "Greatest Hits" ,"Al Green", 1975);
+
+console.log(myCollection, "II", "Boyz II Men", 1991);
+console.log(myCollection, "Here My Dear", "Marvin Gaye", 1978);
+console.log(myCollection, "CTRL", "SZA", 2017);
+console.log(myCollection, "400 Degreez", "Juvenile", 1998);
+console.log(myCollection, "BLACKsummers night", "Maxwell", 2009);
+console.log(myCollection, "Greatest Hits" ,"Al Green", 1975);
+
+console.log(myCollection);
+
+function showCollection(collection) {
+  for (let album of collection) {
+      console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`);
+  }
+}
+
+showCollection(myCollection);
+
+function findByArtist(collection, artist) {
+  let matchingAlbums = [];
+
+  for (let album of collection) {
+      if (album.artist === artist) {
+          matchingAlbums.push(album);
+      }
+  }
+
+  return matchingAlbums;
+}
+
+
+let artist1Albums = findByArtist(myCollection, "SZA");
+console.log(`Albums by SZA:`);
+showCollection(artist1Albums);
+
+
+let artistNotInCollection = findByArtist(myCollection, "SZA");
+console.log(`Albums by SZA:`);
+showCollection(artistNotInCollection);
 
 
 
